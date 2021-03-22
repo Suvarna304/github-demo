db40509f439ca42deaeeee4922277922b0c42891
suvarna <suvarnasolase20@gmail.com>
2021-03-22 16:36:20 +0530

    Fuction headers added

--- math.py
+++ math.py
@@ -1,10 +1,14 @@
 from Pallavi Jog to All Participants:
+#Add implementation
 def add(x,y):
     return x+y
+#Sub implementation
 def subtract(x,y):
     return x-y
+#Multiply implementation
 def multiply(x,y):
     return x*y
+#Division implementation
 def divide(x,y):
 		return x/y
 #Sqaure implementation
